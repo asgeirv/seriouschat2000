e93c977762fdbe24d2adc53d2c250e95c954dade
Asgeir Vinkenes <asgeir.vinkenes@gmail.com>
2017-09-20 13:18:14 +0200

    cleaning up chat stuff

--- SeriousChat2000/src/main/webapp/chat.js
+++ SeriousChat2000/src/main/webapp/chat.js
@@ -31,12 +31,36 @@ class Chat {
         this.worker = new Worker("worker.js");
         this.worker.postMessage({"name": this.name});
 
+        // Stuff to run when Enter is pressed
         this.worker.onmessage = event => {
             this.chat.innerHTML = '';
             let article = document.createElement('article');
-            event.data.map(message => {
+            event.data.map(message =>
+            {
+                var msg = message.text;
+                var wordList = msg.split(" ");
+
+                // Look if message contains a URL
+                for (let i = 0; i < wordList.length; i++)
+                {
+                    var word = wordList[i];
+                    var firstFourLetters = word.substring(0, 4).toLowerCase();
+                    if (firstFourLetters === "http")
+                    {
+                        // Look if URL contains a picture file extension
+                        var jpg = word.search('.jpg');
+                        var jpeg = word.search('.jpeg');
+                        var png = word.search('.png');
+                        if (jpg !== -1 || jpeg !== -1 || png !== -1)
+                        {
+                            msg = msg.replace(word, '<br><a href="' + word + '" target="_blank"><img src="' + word + '"></a>');
+                        }
+                    }
+                }
+
+                // Print out the chat message
                 let chatMessage = document.createElement('p');
-                chatMessage.innerHTML = `${message.user}: ${message.text}`;
+                chatMessage.innerHTML = `${message.user}` + ': ' + msg;
                 article.appendChild(chatMessage);
             });
             this.chat.appendChild(article);
